e986db7ace19dd69673c845bc867871e8e9d9019
Taya Morinville <149643813+tmorinville@users.noreply.github.com>
2024-04-17 11:06:32 -0500

    4/17/2024

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,20 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ChildComponent } from './child/child.component';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, ChildComponent],
+  imports: [RouterOutlet, ChildComponent, FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  
+  pText:string = "";
+  receivedChildMessage:string = "";
+
+  receiveMessage(msg:string){
+    this.receivedChildMessage = msg;
+  }
 }
--- src/app/child/child.component.ts
+++ src/app/child/child.component.ts
@@ -1,12 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
-  selector: 'app-child',
+  selector: 'app-childcomponent',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './child.component.html',
   styleUrl: './child.component.css'
 })
 export class ChildComponent {
+  @Input() inputText:string = "";
+  @Output() outputEvent: EventEmitter<string> = new EventEmitter<string>();
 
+  message:string = "";
+
+  sendMessage(){
+    this.outputEvent.emit(this.message);
+  }
 }
